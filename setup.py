a2b911b71487c64ee0b7ca47d359af8b15fe112a
Kelly Boothby <boothby@dwavesys.com>
2020-06-19 12:06:19 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dissert',
+      version='1.0',
+      description='Granular assertion-disabling via content encodings',
+      url='https://github.com/boothby/dissert',
+      author='Kelly Boothby',
+      license='MIT',
+      packages=['dissert'],
+      zip_safe=True)
